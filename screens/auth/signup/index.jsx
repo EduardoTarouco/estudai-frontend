6cb7fddb2c253fb234e3778cf86774099e0d391d
Eduardo Tarouco <eduardotarouco0@gmail.com>
2025-08-07 20:35:25 -0300

    feat: adiciona o hook react-hook-form

--- screens/auth/signup/index.jsx
+++ screens/auth/signup/index.jsx
@@ -1,25 +1,25 @@
-import {
-  Button,
-  ButtonText
-} from "@/components/ui/button";
+
+import { Button, ButtonText } from "@/components/ui/button";
+import { Heading } from '@/components/ui/heading';
+import { Text } from '@/components/ui/text';
 import { router } from "expo-router";
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { View } from "react-native";
 
 export const SignUp = () => {
-  
-  return (
-    <View className="h-screen w-screen flex justify-center items-center gap-2 bg-yellow-200">
-      <Text className="text-xl font-bold">Signup working!</Text>
+  const [submittedData, setSubmittedData] = useState(null);
+  const onSubmit = info => {
+    setSubmittedData(data);
+    console.log("Submitted Info: " + info);
+  };
+  return <View className="flex-1 flex justify-center items-center gap-2 bg-yellow-200 p-5">
+    <Heading size={"4xl"}>Cadastro</Heading>
+    <Text>Cadastre-se e começe a utilizar o Estudai</Text>
 
-      <Button
-        action={"primary"}
-        variant={"solid"}
-        size={"md"}
-        isDisabled={false}
-        onPress={() => {router.replace("home")}}
-      >
-        <ButtonText>Enviar</ButtonText>
-      </Button>
-    </View>
-  );
-}
+    <Button action={"primary"} variant={"solid"} size={"md"} isDisabled={false} onPress={() => {
+      router.replace("home");
+    }}>
+      <ButtonText>Enviar</ButtonText>
+    </Button>
+  </View>;
+};
