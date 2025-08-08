2ba398d03a62beab8fa3ac52a4c59a79fe96d47e
Eduardo Duarte Tarouco <eduardotarouco0@gmail.com>
2025-08-08 18:05:02 -0300

    feat: melhorado os visuais básicos do cadastro e adicionados os inputs necessários

--- screens/auth/signup/index.jsx
+++ screens/auth/signup/index.jsx
@@ -1,7 +1,7 @@
 import { Button, ButtonText } from "@/components/ui/button";
 import { FormControl } from "@/components/ui/form-control";
 import { Heading } from '@/components/ui/heading';
-import { EyeIcon, EyeOffIcon } from "@/components/ui/icon";
+import { EyeIcon, EyeOffIcon, CalendarDaysIcon, AtSignIcon, MailIcon, LockIcon } from "@/components/ui/icon";
 import { Input, InputField, InputIcon, InputSlot } from "@/components/ui/input";
 import { Text } from '@/components/ui/text';
 import { VStack } from '@/components/ui/vstack';
@@ -23,35 +23,68 @@ export const SignUp = () => {
     console.log("Submitted Info: " + data);
   };
 
-  return <View className="flex-1 flex justify-center items-center gap-2 bg-yellow-200 p-5">
-    <VStack className="self-start">
-      <Heading size={"4xl"}>Cadastro</Heading>
-      <Text>Cadastre-se e começe a utilizar o Estudai</Text>
-    </VStack>
-
-    <FormControl className="p-4 border rounded-lg border-outline-300">
-      <VStack space="xl">
-        <Heading className="text-typography-900">Login</Heading>
-        <VStack space="xs">
-          <Text className="text-typography-500">Email</Text>
-          <Input variant="rounded" size="xl" className="min-w-[250px]">
-            <InputField type="text" />
-          </Input>
-        </VStack>
-        <VStack space="xs">
-          <Text className="text-typography-500">Password</Text>
-          <Input variant="rounded" size="xl" className="text-center">
-            <InputField type={showPassword ? "text" : "password"} />
-            <InputSlot className="pr-3" onPress={() => {setShowPassword(!showPassword)}}>
-              <InputIcon as={showPassword ? EyeIcon : EyeOffIcon} />
-            </InputSlot>
-          </Input>
-        </VStack>
-        <Button action={"primary"} variant={"solid"} size={"md"} isDisabled={false} 
-        onPress={ handleSubmit }>
-          <ButtonText>Enviar</ButtonText>
-        </Button>
+  return (
+    <View className="flex-1 flex justify-center items-center gap-2 bg-yellow-200 p-5">
+      <VStack className="self-start">
+        <Heading size={"4xl"}>Cadastro</Heading>
+        <Text>Cadastre-se e começe a utilizar o Estudai</Text>
       </VStack>
-    </FormControl>    
-  </View>;
+
+      <FormControl className="p-4 border rounded-lg border-outline-300">
+        <VStack space="xl">
+          <VStack space="xs">
+            <Text className="text-typography-500">Nome</Text>
+            <Input variant="rounded" size="xl" className="min-w-[250px]">
+              <InputIcon as={AtSignIcon} className="m-3 -mr-1" />
+              <InputField type="text" placeholder="John Jones" />
+            </Input>
+          </VStack>
+
+          <VStack space="xs">
+            <Text className="text-typography-500">Email</Text>
+            <Input variant="rounded" size="xl" className="min-w-[250px]">
+              <InputIcon as={MailIcon} className="m-3 -mr-1" />
+              <InputField type="text" placeholder="John@gmail.com" />
+            </Input>
+          </VStack>
+
+          <VStack space="xs">
+            <Text className="text-typography-500">Senha</Text>
+            <Input variant="rounded" size="xl" className="text-center">
+              <InputIcon as={LockIcon} className="m-3 -mr-1" />
+              <InputField type={showPassword ? "text" : "password"} placeholder="Senha" />
+              <InputSlot className="pr-3" onPress={() => {setShowPassword(!showPassword)}}>
+                <InputIcon as={showPassword ? EyeIcon : EyeOffIcon} />
+              </InputSlot>
+            </Input>
+          </VStack>
+
+          <VStack space="xs">
+            <Text className="text-typography-500">Confirme a senha</Text>
+            <Input variant="rounded" size="xl" className="text-center">
+              <InputIcon as={LockIcon} className="m-3 -mr-1" />
+              <InputField type={showPassword ? "text" : "password"} placeholder="Repetir senha" />
+              <InputSlot className="pr-3" onPress={() => {setShowPassword(!showPassword)}}>
+                <InputIcon as={showPassword ? EyeIcon : EyeOffIcon} />
+              </InputSlot>
+            </Input>
+          </VStack>
+
+          <VStack space="xs">
+            <Text className="text-typography-500">Data de nascimento</Text>
+            <Input variant="rounded" size="xl" className="text-center">
+              <InputIcon as={CalendarDaysIcon} className="m-3 -mr-1" />
+              <InputField type="text" placeholder="01/01/2000" />
+            </Input>
+          </VStack>
+
+          <Button action={"primary"} variant={"solid"} size={"md"} isDisabled={false} 
+          onPress={ handleSubmit }>
+            <ButtonText>Enviar</ButtonText>
+          </Button>
+
+        </VStack>
+      </FormControl>    
+    </View>
+  );
 };
