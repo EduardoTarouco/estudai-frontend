60ca9f1f811d8fd7908ccdb01d9ac8e8ac179d13
Eduardo Tarouco <eduardotarouco0@gmail.com>
2025-09-09 21:46:18 -0300

    refactor: substitui o método de redirecionamento da página 404

--- app/+not-found.jsx
+++ app/+not-found.jsx
@@ -1,19 +1,19 @@
-import { Link, Stack } from "expo-router";
-import { Text, View } from "react-native";
+import { Text, View, Pressable } from "react-native";
+import { useRouter } from "expo-router";
 
 export default function index() {
+  const router = useRouter();
 
   return (
-    <>
-      <Stack.Screen options={{ title: "Oops!" }} />
+    <View>
       <View className="h-screen w-screen flex justify-center items-center p-12">
         <Text className="text-6xl font-extrabold">404</Text>
         <Text className="text-[20px] font-bold">Esta tela não existe!</Text>
-
-        <Link href="/" className="">
-          <Text className="text-[14px] text-blue-500 underline">Voltar a página principal</Text>
-        </Link>
+        
+        <Pressable onPress={() => router.push('/home')}>
+          <Text className="text-[14px] text-blue-500 underline">Voltar à página principal</Text>
+        </Pressable>
       </View>
-    </>
+    </View>
   );
 };
