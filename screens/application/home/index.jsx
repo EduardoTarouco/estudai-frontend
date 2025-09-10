6fe0385e3a9979698e25b4920423f2ece22a98b7
Eduardo Tarouco <eduardotarouco0@gmail.com>
2025-09-09 21:47:55 -0300

    feat: inicia implementação da tela de questões bem como sua navegação a partir da home

--- screens/application/home/index.jsx
+++ screens/application/home/index.jsx
@@ -1,17 +1,19 @@
 import { EstudaiHeader } from "@/components/application/EstudaiHeader";
+import { SubjectGroup } from "@/components/application/SubjectGroup";
 import { Button, ButtonText } from "@/components/ui/button";
 import { useSession } from "@/contexts/AuthContext";
 import { VStack } from '@/components/ui/vstack';
 import { Text, View } from "react-native";
 
 export const Home = () => {
-  
+
   const { signOut } = useSession();
 
   return (
     <View className="flex-1">
       <EstudaiHeader />
-      <VStack space={"md"} className="flex-1 justify-center items-center">
+      <VStack space={"md"} className="flex-1 p-5 items-center">
+        <SubjectGroup />
         <Text className="text-2xl text-center">Home screen working...</Text>
         <Button
           action={"secondary"} 
--- screens/application/questionList/index.jsx
+++ screens/application/questionList/index.jsx
@@ -1,13 +1,18 @@
 import { EstudaiHeader } from "@/components/application/EstudaiHeader";
+import { useLocalSearchParams } from "expo-router";
 import { View, Text } from "react-native";
 
 export const QuestionList = () => {
+
+  const params = useLocalSearchParams();
+  console.log(params);
 
   return (
     <View className="flex-1">
       <EstudaiHeader />
       <View className="justify-center items-center flex-1">
         <Text className="text-2xl text-center">Question list screen working...</Text>
+        <Text>Params: {JSON.stringify(params)}</Text>
       </View>
     </View>
   );
