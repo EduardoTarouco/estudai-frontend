0c45162c8c506282399b1e54ee588d39632020aa
Eduardo Tarouco <eduardotarouco0@gmail.com>
2025-09-10 21:25:08 -0300

    refactor: recebe o valor de subject vinda da home diretamente dos parametros locais de pesquisa

--- screens/application/questionList/index.jsx
+++ screens/application/questionList/index.jsx
@@ -4,15 +4,15 @@ import { View, Text } from "react-native";
 
 export const QuestionList = () => {
 
-  const params = useLocalSearchParams();
-  console.log(params);
+  const { subject } = useLocalSearchParams();
+  console.log(subject);
 
   return (
     <View className="flex-1">
       <EstudaiHeader />
       <View className="justify-center items-center flex-1">
         <Text className="text-2xl text-center">Question list screen working...</Text>
-        <Text>Params: {JSON.stringify(params)}</Text>
+        <Text>Params: {subject}</Text>
       </View>
     </View>
   );
