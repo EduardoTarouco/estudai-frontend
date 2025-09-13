d2fdc32ad7d1b06dcda26138ce40c66064cb535a
Eduardo Tarouco <eduardotarouco0@gmail.com>
2025-09-12 21:23:57 -0300

    refactor: adiciona props de config ao título e cor do QuestionListHeader

--- components/application/headers/QuestionListHeader/index.jsx
+++ components/application/headers/QuestionListHeader/index.jsx
@@ -2,11 +2,11 @@ import { SubjectItem } from '@/components/application/SubjectItem';
 import { HStack } from '@/components/ui/hstack';
 import { Text } from "react-native";
 
-export const QuestionListHeader = () => {
+export const QuestionListHeader = ({ title, color = "default" }) => {
 
   return (
     <HStack space="lg" className="bg-black w-full justify-between items-center p-4 px-6">
-      <SubjectItem color={"default"} size={"md"} title={"MAT"} />
+      <SubjectItem color={color} size={"md"} title={title} />
       <Text className="text-3xl font-bold color-white">ESTUDAÍ</Text>
     </HStack>
   );
--- screens/application/questionList/index.jsx
+++ screens/application/questionList/index.jsx
@@ -4,14 +4,13 @@ import { View, Text } from "react-native";
 
 export const QuestionList = () => {
 
-  const { subject } = useLocalSearchParams();
+  const { title, color } = useLocalSearchParams();
 
   return (
     <View className="flex-1">
-      <QuestionListHeader />
+      <QuestionListHeader title={title} color={color} />
       <View className="justify-center items-center flex-1">
         <Text className="text-2xl text-center">Question list screen working...</Text>
-        <Text>Params: {subject}</Text>
       </View>
     </View>
   );
