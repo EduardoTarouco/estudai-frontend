6a31fa2c63d5b323f1ba2d8f5d3ca0e0a3edbffd
Eduardo Duarte Tarouco <eduardotarouco0@gmail.com>
2025-10-31 16:41:44 -0300

    feat: implementa o componente de input de seleção

--- components/application/SelectButton/index.jsx
+++ components/application/SelectButton/index.jsx
@@ -1,38 +1,26 @@
-import {
-  Select,
-  SelectTrigger,
-  SelectInput,
-  SelectIcon,
-  SelectPortal,
-  SelectBackdrop,
-  SelectContent,
-  SelectDragIndicatorWrapper,
-  SelectDragIndicator,
-  SelectItem,
-} from '@/components/ui/select';
-import { ChevronDown } from "lucide-react-native";
+import { Picker } from '@react-native-picker/picker';
+import { VStack } from '@/components/ui/vstack';
+import { Text } from 'react-native';
 
-export const SelectButton = ({ placeholder, disabled=false, IconComponent=ChevronDown }) => {
+export const SelectButton = ({ setSelected, selectedValue, title="Escolha", items=["Sem itens"], enabled=true }) => {
 
   return (
-    <Select>
-      <SelectTrigger variant="outline" size="md" >
-        <SelectInput placeholder={placeholder} className="text-md" />
-        <SelectIcon className="mr-3" as={IconComponent} />
-      </SelectTrigger>
-      <SelectPortal>
-        <SelectBackdrop />
-        <SelectContent>
-          <SelectDragIndicatorWrapper>
-            <SelectDragIndicator />
-          </SelectDragIndicatorWrapper>
-          <SelectItem label="2019" value="2019" />
-          <SelectItem label="2020" value="2020" />
-          <SelectItem label="2021" value="2021" />
-          <SelectItem label="2022" value="2022" isDisabled={true} />
-          <SelectItem label="2023" value="2023" />
-        </SelectContent>
-      </SelectPortal>
-    </Select>
+    <VStack space="sm">
+      <Text className="text-xl font-bold">{title}:</Text>
+      <Picker
+        selectedValue={selectedValue}
+        onValueChange={(itemValue, itemIndex) =>
+          setSelected(itemValue)
+        }
+        enabled={enabled}
+        style={{ width: 200, height: 50, color: 'black', backgroundColor: 'lightgray', borderRadius: 32 }} 
+      >
+      {
+        items.map((item, index) => (
+          <Picker.Item key={index} label={item} value={item} />
+        ))
+      }
+      </Picker>
+    </VStack>
   );
 }
