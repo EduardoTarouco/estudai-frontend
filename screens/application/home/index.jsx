989eb1826932a0b562d72733bb16c6411f7b90c0
Eduardo Duarte Tarouco <eduardotarouco0@gmail.com>
2025-12-03 23:51:44 -0300

    feat: componente de streak implementado

--- screens/application/home/index.jsx
+++ screens/application/home/index.jsx
@@ -1,9 +1,10 @@
 import { EstudaiHeader } from "@/components/application/headers/EstudaiHeader";
 import { SubjectGroup } from "@/components/application/SubjectGroup";
+import { StreakBadge } from "@/components/application/StreakBadge";
 import { Button, ButtonText } from "@/components/ui/button";
 import { useSession } from "@/contexts/AuthContext";
 import { VStack } from '@/components/ui/vstack';
-import { Text, View } from "react-native";
+import { View } from "react-native";
 
 export const Home = () => {
 
@@ -14,12 +15,12 @@ export const Home = () => {
       <EstudaiHeader />
       <VStack space={"md"} className="flex-1 p-5 items-center">
         <SubjectGroup size={"xl"} />
-        <Text className="text-2xl text-center">Home screen working...</Text>
+        <StreakBadge/>
         <Button
           action={"secondary"} 
           variant={"solid"} 
           size={"sm"} 
-          onPress={async () => {await signOut()}}
+          onPress={async () => { await signOut() }}
         >
           <ButtonText>Log-out</ButtonText>
         </Button>
